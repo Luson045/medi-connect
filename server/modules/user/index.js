85ac0647ef3e8969a246bccee9cd8452a4565dc5
Somnath Chattaraj <somnathchattaraj5@gmail.com>
2024-10-02 12:14:15 +0530

    Update emergency route to find the nearest hospital and return appointment details
    Co-authored-by: Somnath Chattaraj somnathchattaraj5@gmail.com
    Co-authored-by: Tanish Majumdar tanishmajumdar2912@gmail.com

--- server/modules/user/index.js
+++ server/modules/user/index.js
@@ -38,6 +38,7 @@ router.get('/', async (req, res) => {
         const users = await User.find();
         res.send(users);
     } catch (error) {
+        
         res.status(500).send(error);
     }
 });
