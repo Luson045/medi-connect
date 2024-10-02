29a4080949c497a4d6fa7ecb3e6405bd59e2030a
lade6501 <lade6501@gmail.com>
2024-10-02 18:28:04 +0530

    feat: Added SMS notification

--- server/modules/hospital/index.js
+++ server/modules/hospital/index.js
@@ -7,8 +7,8 @@ const axios = require("axios");
 const jwt = require("jsonwebtoken");
 require("dotenv").config({ path: "../.env" });
 const { z } = require("zod");
-const { sendMail } = require("../mail/sendMail");
-
+const { sendMail } = require("../notifications/sendMail");
+const sendSMS = require("../notifications/sendSMS");
 
 const router = express.Router();
 const jwtSecret = process.env.JWT;
@@ -356,6 +356,11 @@ router.post(
       await sendMail(
         `Your appointment has been booked at ${hospital.name} on ${appointment.date}`,
         profile.email
+      );
+      /* Sending SMS confirmation */
+      sendSMS(
+        `Your appointment has been booked at ${hospital.name} on ${appointment.date}`,
+        `+91${profile.phone}`
       );
       res.status(200).json({
         message: "Appointment booked successfully",
