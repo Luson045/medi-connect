ec9776bbc5b94816f4313d29c3f1f951e125ff9e
shiba sarkar <shibasarkar1994@gmail.com>
2024-10-02 20:54:10 +0530

    Design Login page with basic functionality

--- client/src/modules/common/AuthForm.jsx
+++ client/src/modules/common/AuthForm.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
 import Navbar from '../common/Navbar';
 import { notify } from './notification';
-
+import '../../styles/AuthForm.css'
+import { Link } from 'react-router-dom';
+import {FaEyeSlash ,FaEye} from 'react-icons/fa';
 const AuthPage = () => {
     const [isRegistering, setIsRegistering] = useState(false);
     const [formData, setFormData] = useState({
@@ -15,6 +17,7 @@ const AuthPage = () => {
         dateOfBirth: '',
         gender: 'male',
     });
+	const [type, setType] = useState('password');
 
     const toggleAuthMode = () => {
         setIsRegistering(!isRegistering);
@@ -26,7 +29,15 @@ const AuthPage = () => {
             [e.target.name]: e.target.value,
         });
     };
-
+    const handleToggle = () => {
+		if (type==='password'){
+		 //  setIcon('eye');
+		   setType('text')
+		} else {
+		  // setIcon('eye-slash')
+		   setType('password')
+		}
+	 }
     const handleSubmit = async (e) => {
         e.preventDefault();
 
@@ -77,105 +88,119 @@ const AuthPage = () => {
     return (
       <>
         <Navbar />
-        <div className="auth-container">
-            <h2>{isRegistering ? 'Register' : 'Login'}</h2>
+        <div className='auth-container'>
+				<h2>{isRegistering ? 'Register' : 'Login'}</h2>
 
-            {/* Form Section */}
-            <form onSubmit={handleSubmit} className="auth-form">
-                <div className="form-section">
-                    <label>User Type:</label>
-                    <select name="type" value={formData.type} onChange={handleChange}>
-                        <option value="user">User</option>
-                        <option value="hospital">Hospital</option>
-                    </select>
-                </div>
+				{/* Form Section */}
+				<form onSubmit={handleSubmit} className='auth-form'>
+					<div className='form-group'>
+						<label>User Type:</label>
+						<select name='type' value={formData.type} onChange={handleChange}>
+							<option value='user'>User</option>
+							<option value='hospital'>Hospital</option>
+						</select>
+					</div>
 
-                {isRegistering && (
-                    <>
-                        <div className="form-section">
-                            <label>Name:</label>
+					{isRegistering && (
+						<>
+							<div className='form-group'>
+								<label>Name:</label>
+								<input
+									type='text'
+									name='name'
+									value={formData.name}
+									onChange={handleChange}
+									required
+								/>
+							</div>
+
+							<div className='form-group'>
+								<label>Phone:</label>
+								<input
+									type='text'
+									name='phone'
+									value={formData.phone}
+									onChange={handleChange}
+									
+								/>
+							</div>
+
+							<div className='form-group'>
+								<label>Address:</label>
+								<input
+									type='text'
+									name='address'
+									value={formData.address}
+									onChange={handleChange}
+									
+								/>
+							</div>
+						</>
+					)}
+
+					<div className='form-group'>
+						<label>Email:</label>
+						<input
+							type='text'
+							name='email'
+                            placeholder='example@mail.com'
+							value={formData.email}
+							onChange={handleChange}
+							
+						/>
+					</div>
+
+					<div className='form-group'>
+						<label>Password:</label>
+						<div className='input-group'>
                             <input
-                                type="text"
-                                name="name"
-                                value={formData.name}
+                                type={type}
+                                name='password'
+                                placeholder='password'
+                                value={formData.password}
                                 onChange={handleChange}
-                                required
+                               
                             />
-                        </div>
+                            <span className="eye-button" onClick={handleToggle}>
+                            {type==='password' ?  <FaEyeSlash className="absolute mr-10" size = {25}/>:<FaEye className="absolute mr-10"  size = {25}/> } 
+                            </span>
+						</div>
+						
+					</div>
 
-                        <div className="form-section">
-                            <label>Phone:</label>
-                            <input
-                                type="text"
-                                name="phone"
-                                value={formData.phone}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
+					{isRegistering && (
+						<div className='form-section'>
+							<label>Confirm Password:</label>
+							<input
+								type='password'
+								name='confirmPassword'
+								value={formData.confirmPassword}
+								onChange={handleChange}
+								
+							/>
 
-                        <div className="form-section">
-                            <label>Address:</label>
-                            <input
-                                type="text"
-                                name="address"
-                                value={formData.address}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
+						</div>
+						
+						
+					)}
 
-
-
-                    </>
-                )}
-
-                <div className="form-section">
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        name="email"
-                        value={formData.email}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-section">
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        name="password"
-                        value={formData.password}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-
-                {isRegistering && (
-                    <div className="form-section">
-                        <label>Confirm Password:</label>
-                        <input
-                            type="password"
-                            name="confirmPassword"
-                            value={formData.confirmPassword}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                )}
-
-                <button type="submit" className="auth-button">
-                    {isRegistering ? 'Register' : 'Login'}
-                </button>
-            </form>
-
-            <button onClick={toggleAuthMode} className="toggle-auth-button">
-                {isRegistering
-                    ? 'Already have an account? Login'
-                    : "Don't have an account? Register"}
-            </button>
-        </div>
+					<button type='submit' className='auth-button'>
+						{isRegistering ? 'Register' : 'Login'}
+					</button>
+				</form>
+				<div className='remember-me'>
+				<div className='remember-me-chk remember-me'>
+				<input type='checkbox'/> Remember me
+				</div>
+				<Link to="#" className="back-button">Forgot password?</Link>
+				</div>
+				<button onClick={toggleAuthMode} className='toggle-auth-button'>
+					{isRegistering
+						? 'Already have an account? Login'
+						: "Don't have an account? Register"}
+				</button>
+				
+			</div>
       </>
     );
 };
