4759a454bd81dcb1a5e9f309bdc06d19ec22b88f
Nawin <nawinsharma60@gmail.com>
2024-10-04 00:24:52 +0530

    feat: add a Custom Cursor

--- client/src/App.js
+++ client/src/App.js
@@ -12,10 +12,25 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Footer from '../src/modules/common/Footer'; // Import Footer
+import AnimatedCursor from 'react-animated-cursor';
 
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        hasBlendMode={true}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)',
+        }}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)',
+        }}
+      />{' '}
       <Router>
         <UserProvider>
           <div>
