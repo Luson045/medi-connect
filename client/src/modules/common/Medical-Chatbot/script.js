4c961381964c1c86c1b635668a743635774343ec
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:31:08 +0530

    Add chatbox UI elements and functionality to Medical-Chatbot frontend

---
+++ client/src/modules/common/Medical-Chatbot/script.js
@@ -0,0 +1,10 @@
+const chatbotToggler = document.querySelector(".chatbot-toggler");
+const closeBtn = document.querySelector(".close-btn");
+const chatbox = document.querySelector(".chatbox");
+const chatInput = document.querySelector(".chat-input textarea");
+const sendChatBtn = document.querySelector(".chat-input span");
+
+let userMessage = null; 
+const inputInitHeight = chatInput.scrollHeight;
+
+
