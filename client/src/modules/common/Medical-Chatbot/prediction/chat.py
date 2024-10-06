5b70419a217d15018722ca1aae188e07a5498d20
arpita <arpitaagrahari004@gmail.com>
2024-10-06 22:34:28 +0530

    Refactor chat.py to include prediction functionality

--- client/src/modules/common/Medical-Chatbot/prediction/chat.py
+++ client/src/modules/common/Medical-Chatbot/prediction/chat.py
@@ -35,3 +35,15 @@ def bag_of_words(sentence):
         bag[i]=1
   return np.array(bag)
 
+def predict_class(sentence):
+  bow=bag_of_words(sentence)
+  res=model.predict(np.array([bow]))[0]
+  ERROR_THRESHOLD=0.25
+  results=[[i,r] for i,r in enumerate(res) if r> ERROR_THRESHOLD]
+
+  results.sort(key=lambda x:x[1],reverse=True)
+  return_list=[]
+  for r in results:
+    return_list.append({'intent': classes[r[0]],'probability':str(r[1])})
+  return return_list
+
