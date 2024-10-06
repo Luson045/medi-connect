e0ac1196fc9058f78d8b93ca8da9eeb5487e6392
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:08:34 +0530

    Add greetings and goodbye intents to Medical-Chatbot prediction module

---
+++ client/src/modules/common/Medical-Chatbot/prediction/intents.py
@@ -0,0 +1,14 @@
+{
+    "intents": [
+          {
+             "tag": "greetings",
+             "patterns": [ "hello", "hey", "hi", "good day", "greetings", "what's up?", "how is it going" ],
+             "responses": [ "hello", "hey!", "what can i do for you?" ],
+             "context": [ "" ]
+          },
+          {
+             "context": [ "" ],
+             "patterns": [ "cya", "see you later", "goodbye", "have a good day", "bye", "ciao", "see ya" ],
+             "responses": [ "have a nice day", "goodbye" ],
+             "tag": "goodbye"
+          },
