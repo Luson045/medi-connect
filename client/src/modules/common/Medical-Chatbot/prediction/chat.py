aaa4e1458a8b84c85912eb2432ec54051cbd2e49
arpita <arpitaagrahari004@gmail.com>
2024-10-06 22:33:36 +0530

    Add functions for cleaning up sentences and creating a bag of words

--- client/src/modules/common/Medical-Chatbot/prediction/chat.py
+++ client/src/modules/common/Medical-Chatbot/prediction/chat.py
@@ -20,3 +20,18 @@ words=pickle.load(open('words.pkl','rb'))
 classes=pickle.load(open('classes.pkl','rb'))
 model=load_model('chatbotmodel.h5')
 
+
+def clean_up_sentence(sentence):
+  sentence_words=nltk.word_tokenize(sentence)
+  sentence_words=[lemmatizer.lemmatize(word) for word in sentence_words]
+  return sentence_words
+
+def bag_of_words(sentence):
+  sentence_words=clean_up_sentence(sentence)
+  bag=[0]*len(words)
+  for w in sentence_words:
+    for i,word in enumerate(words):
+      if word == w:
+        bag[i]=1
+  return np.array(bag)
+
