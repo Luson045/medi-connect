a65700c7a50edb4b8296495498a19d8b0b712d23
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:28:07 +0530

    Add code for training the Medical-Chatbot with lemmatization, bag-of-words encoding, and intent patterns

--- client/src/modules/common/Medical-Chatbot/training-chatbot/train_chatbot.py
+++ client/src/modules/common/Medical-Chatbot/training-chatbot/train_chatbot.py
@@ -57,3 +57,22 @@ for document in documents:
   output_row[classes.index(document[1])]=1
   training.append([bag,output_row])
 
+random.shuffle(training)
+training=np.array(training)
+
+train_x=list(training[:,0])
+train_y=list(training[:,1])
+model=Sequential()
+model.add(Dense(128,input_shape=(len(train_x[0]),),activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64,activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]),activation='softmax'))
+
+sgd=SGD(lr=0.01,decay=1e-6,momentum=0.9,nesterov=True)
+model.compile(loss='categorical_crossentropy',optimizer=sgd,metrics=['accuracy'])
+hist = model.fit(np.array(train_x),np.array(train_y),epochs=200,batch_size=5,verbose=1)
+model.save('chatbotmodel.h5', hist)
+print('Training Done')
+
+
