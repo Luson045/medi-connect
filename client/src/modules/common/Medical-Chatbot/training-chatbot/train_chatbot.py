341caf5c6c0255fe76728b1f164d6ea7fc1662e8
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:27:13 +0530

    Add intent patterns and tags for Medical-Chatbot training

--- client/src/modules/common/Medical-Chatbot/training-chatbot/train_chatbot.py
+++ client/src/modules/common/Medical-Chatbot/training-chatbot/train_chatbot.py
@@ -26,3 +26,12 @@ classes=[]
 documents=[]
 ignore_letters=['?','!','.',',']
 
+for intent in intents['intents']:
+  for pattern in intent['patterns']:
+    word_list=nltk.word_tokenize(pattern)
+    words.extend(word_list)
+    documents.append((word_list,intent['tag']))
+    if intent['tag'] not in classes:
+      classes.append(intent['tag'])
+
+
