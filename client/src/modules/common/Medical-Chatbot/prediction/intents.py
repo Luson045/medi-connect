1e76326f8a90d06a10007387602a5aae66fe9362
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:09:02 +0530

    Add greetings and goodbye intents to Medical-Chatbot prediction module

--- client/src/modules/common/Medical-Chatbot/prediction/intents.py
+++ client/src/modules/common/Medical-Chatbot/prediction/intents.py
@@ -12,3 +12,16 @@
              "responses": [ "have a nice day", "goodbye" ],
              "tag": "goodbye"
           },
+          {
+             "context": [ "" ],
+             "patterns": [ "how old", "how old are you?", "what is your age", "how old are you", "age?" ],
+             "responses": [ "I get reborn after every compilation", "hey!", "my owners are averagely 20 years!" ],
+             "tag": "age"
+          },
+          {
+             "context": [ "" ],
+             "patterns": [ "what is your name", "what should i call you", "what's your name?", "who are you?", "can you tell me your name" ],
+             "responses": [ "you can call me Medbot!", "i am Medbot!", "i am Medbot your medical assistant" ],
+             "tag": "name"
+          },
+  
