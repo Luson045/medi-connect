f091e7a6f14fec61832b8aed15d7718ec003183f
arpita <arpitaagrahari004@gmail.com>
2024-10-06 23:31:28 +0530

    Add config file loading functionality to Medical-Chatbot frontend

--- client/src/modules/common/Medical-Chatbot/script.js
+++ client/src/modules/common/Medical-Chatbot/script.js
@@ -8,3 +8,17 @@ let userMessage = null;
 const inputInitHeight = chatInput.scrollHeight;
 
 
+let API_KEY = ''; // Your API key here
+let API_URL = '';
+let intents=[];
+
+const loadConfig = async () => {
+  try {
+    const response = await fetch('config.json');
+    const config = await response.json();
+    API_KEY = config.apiKey;
+    API_URL = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${API_KEY}`;
+  } catch (error) {
+    console.error('Error loading config:', error);
+  }
+};
