4eac6c27d55cde799b8f0c334c14e294354ac1bc
arpita <arpitaagrahari004@gmail.com>
2024-10-06 22:30:13 +0530

    Add nltk downloads and imports for Medical-Chatbot prediction module

---
+++ client/src/modules/common/Medical-Chatbot/prediction/chat.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('wordnet')
+import random
+import numpy as np
+import json
+import pickle
+from nltk.stem import WordNetLemmatizer
+from tensorflow.keras.models import load_model
+nltk.download('punkt')
+nltk.download('omw-1.4')
+
