38f57bb9df99fd65f7edf626c206f3d14ba04ca6
Jay-1409 <jay_shah@outlook.in>
2024-10-09 19:56:54 +0530

    hello world

--- client/src/modules/common/TreatmentModel.jsx
+++ client/src/modules/common/TreatmentModel.jsx
@@ -13,6 +13,8 @@ const TreatmentModal = ({ isOpen, onRequestClose, treatment }) => {
   };
 
   return (
+
+    
     <motion.div
       className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50"
       initial={{ opacity: 0 }}
