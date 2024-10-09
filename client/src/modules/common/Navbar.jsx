ddf9c6d413c43054d3281a98396b31ddd2360421
Mugundh <mugundhjb@gmail.com>
2024-10-09 14:45:45 +0530

    Added blog pages

--- client/src/modules/common/Navbar.jsx
+++ client/src/modules/common/Navbar.jsx
@@ -94,6 +94,16 @@ const Navbar = () => {
             <MdOutlineLocalHospital />{' '}
             <p className="hover:brightness-50 hover:font-semibold">Services</p>
           </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              `${isActive ? 'border-b border-white ' : ''} flex gap-2 items-baseline`
+            }
+            to="/blog"
+            onClick={() => setMobileMenuOpen(false)}
+          >
+            <MdOutlineLocalHospital />{' '}
+            <p className="hover:brightness-50 hover:font-semibold">Blogs</p>
+          </NavLink>
 
           {isAuthenticated ? (
             <>
@@ -201,7 +211,13 @@ const Navbar = () => {
             <MdOutlineLocalHospital />{' '}
             <p className="font-bold text-lg hover:brightness-50">Services</p>
           </NavLink>
-
+          <NavLink
+            to="/blog"
+            className="flex justify-center items-center gap-2"
+          >
+            <MdOutlineLocalHospital />{' '}
+            <p className="font-bold text-lg hover:brightness-50">Blog</p>
+          </NavLink>
           <NavLink to="/labtest" className="flex items-baseline gap-2">
             <MdOutlineLocalHospital /> {' '}
             <p className="font-bold text-lg hover:brightness-50">Lab Test</p>
