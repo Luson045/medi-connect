329d75f54002dea4d91161e950cff23a7a059f2a
lade6501 <lade6501@gmail.com>
2024-10-11 12:52:45 +0530

    Feat: Added progress indicator on scroll

---
+++ client/src/modules/common/ScrollProgressIndicator.jsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+
+import '../../styles/ScrollProgressIndicator.css';
+
+function ScrollProgressIndicator() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+      const scrollPercent = (scrollTop / docHeight) * 100;
+      console.log();
+
+      setScrollPosition(scrollPercent);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <>
+      <div className="progressBar"></div>
+      <div
+        className="progressIndicator"
+        style={{ width: `${scrollPosition}%` }}
+      ></div>
+    </>
+  );
+}
+
+export default ScrollProgressIndicator;
