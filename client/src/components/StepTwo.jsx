1e438e3a76f5f0f3d516ac49facfeb9c013ef976
Luson Basumatary <131430087+Luson045@users.noreply.github.com>
2024-10-13 23:19:37 +0530

    Update StepTwo.jsx
    fixed genders

--- client/src/components/StepTwo.jsx
+++ client/src/components/StepTwo.jsx
@@ -341,9 +341,9 @@ function StepTwo() {
               <option value="" disabled>
                 Select Gender
               </option>
-              <option value="male">Male</option>
-              <option value="female">Female</option>
-              <option value="other">Other</option>
+              <option value="Male">Male</option>
+              <option value="Memale">Female</option>
+              <option value="Other">Other</option>
             </select>
             {errors.frontend.gender && (
               <span className="error">{errors.frontend.gender}</span>
